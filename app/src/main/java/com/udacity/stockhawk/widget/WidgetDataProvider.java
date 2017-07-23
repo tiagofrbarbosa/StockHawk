fcfd61249fa582c190193441889cc03dfd4c33ea
Tiago Barbosa <tfreirebar@gmail.com>
2017-07-23 17:05:28 -0300

    Feat: add quotes on widget

--- app/src/main/java/com/udacity/stockhawk/widget/WidgetDataProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/WidgetDataProvider.java
@@ -1,10 +1,23 @@
 package com.udacity.stockhawk.widget;
 
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.Contract.Quote;
+
+import com.udacity.stockhawk.R;
+
+import com.udacity.stockhawk.data.StockProvider;
+import com.udacity.stockhawk.ui.MainActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +26,10 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     private static final String TAG = "WidgetDataProvider";
 
-    List<String> mCollection = new ArrayList<>();
+    private Cursor data = null;
     Context mContext = null;
+    private ContentResolver mResolver;
+    private Uri mUri;
 
     public WidgetDataProvider(Context context, Intent intent) {
         mContext = context;
@@ -22,7 +37,7 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     @Override
     public void onCreate() {
-        initData();
+        //initData();
     }
 
     @Override
@@ -33,18 +48,49 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     @Override
     public void onDestroy() {
 
+        if(data != null){
+            data.close();
+        }
+
     }
 
     @Override
     public int getCount() {
-        return mCollection.size();
+        if(data == null){
+            return 0;
+        }else{
+            return data.getCount();
+        }
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
-        RemoteViews view = new RemoteViews(mContext.getPackageName(),
-                android.R.layout.simple_list_item_1);
-        view.setTextViewText(android.R.id.text1, mCollection.get(position));
+
+        if (position == AdapterView.INVALID_POSITION ||data == null || !data.moveToPosition(position)) {
+            return null;
+        }
+
+        RemoteViews view = new RemoteViews(mContext.getPackageName(), R.layout.widget_collection_item);
+
+        //view.setTextViewText(android.R.id.text1, mCollection.get(position));
+
+        view.setTextViewText(R.id.stock_symbol, data.getString(data.getColumnIndex("symbol")));
+
+        String bidPrice = data.getString(data.getColumnIndex(Quote.COLUMN_PRICE));
+        String symbol = data.getString(data.getColumnIndex(Quote.COLUMN_SYMBOL));
+        view.setTextViewText(R.id.bid_price, bidPrice);
+        view.setTextViewText(R.id.change, data.getString(4));
+
+
+            view.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+
+        final Intent fillInIntent = new Intent();
+        //final Bundle extras = new Bundle();
+        //extras.putString(QuoteWidgetProvider.EXTRA_QUOTE, symbol);
+        fillInIntent.putExtra("Symbol", symbol);
+        view.setOnClickFillInIntent(R.id.widget_list_item, fillInIntent);
+
+
         return view;
     }
 
@@ -69,10 +115,29 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     }
 
     private void initData() {
-        mCollection.clear();
-        for (int i = 1; i <= 10; i++) {
-            mCollection.add("ListView item " + i);
+
+        if (data != null) {
+            data.close();
         }
+
+        final long token = Binder.clearCallingIdentity();
+
+
+        mResolver = mContext.getContentResolver();
+
+        mUri = Contract.Quote.URI;
+
+
+        String[] projection = new String[]{Contract.Quote._ID,Contract.Quote.COLUMN_SYMBOL, Contract.Quote.COLUMN_PRICE,
+                Contract.Quote.COLUMN_ABSOLUTE_CHANGE, Contract.Quote.COLUMN_PERCENTAGE_CHANGE, Contract.Quote.COLUMN_HISTORY};
+
+        String selection = null;
+        String[] selectionArgs = null;
+        String sortOrder = null;
+
+        data = mResolver.query(mUri, projection, selection, selectionArgs, sortOrder);
+
+        Binder.restoreCallingIdentity(token);
     }
 
 }
